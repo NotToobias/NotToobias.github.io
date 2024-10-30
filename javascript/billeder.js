2470acf47ceb3ad1a4807b44d184daecffd7a3c9
NotToobias <tobiaserkeby@gmail.com>
2024-10-30 20:15:06 +0100

    vbn

--- javascript/billeder.js
+++ javascript/billeder.js
@@ -1,22 +1,28 @@
-
-    // Function to close the lightbox when clicking outside the image
-    function closeLightbox() {
-        document.querySelectorAll('.lightbox').forEach(lightbox => {
-            lightbox.style.display = 'none';
-        });
+   // Function to close the lightbox when clicking outside the image
+   function checkBackgroundClick(event) {
+    // Close lightbox only if the clicked element is the lightbox (background)
+    if (event.target.classList.contains('lightbox')) {
+        closeLightbox();
     }
+}
 
-    // Listen for the escape key press to close the lightbox
-    document.addEventListener('keydown', function(event) {
-        if (event.key === 'Escape') {
-            closeLightbox();
-        }
+// Function to close all lightboxes
+function closeLightbox() {
+    document.querySelectorAll('.lightbox').forEach(lightbox => {
+        lightbox.style.display = 'none';
     });
+}
 
-    // Open the lightbox
-    document.querySelectorAll('.collage-container img').forEach((img, index) => {
-        img.addEventListener('click', function () {
-            document.getElementById('lightbox' + (index + 1)).style.display = 'flex';
-        });
+// Listen for the escape key press to close the lightbox
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Escape') {
+        closeLightbox();
+    }
+});
+
+// Open the lightbox
+document.querySelectorAll('.collage-container img').forEach((img, index) => {
+    img.addEventListener('click', function () {
+        document.getElementById('lightbox' + (index + 1)).style.display = 'flex';
     });
-
+});
